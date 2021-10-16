1f4adae5e10264b80f078ecc10764f09f61d76cd
gabriel scalese <gabriel.scalese@hotmail.com>
2021-10-15 23:32:09 -0300

    Commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./components/Header/Header";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <div className="header-container">
+        <Header />
+      </div>
+      <div className="newsPostsBox">
+        <h1>a</h1>
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Fast News</h1>
+      <div className="line-container"></div>
+    </div>
+  );
+};
+
+export default Header;
